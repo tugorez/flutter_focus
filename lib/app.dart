184698c1a498b74047234106de7643110a08b05f
Juan Tugores <tugorez@google.com>
2024-02-27 09:01:42 -0800

    Better structure the app

---
+++ lib/app.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            for (int i = 0; i < 6; i++)
+              TextButton(
+                onPressed: _incrementCounter,
+                child: Text('Press me to increase count'),
+              ),
+            Text('You have pushed the button this many times: $_counter'),
+            SizedBox(height: 100),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main_with_focus_handlers.dart
+++ lib/main_with_focus_handlers.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'focusable_app.dart';
 
 void main() {
-  runApp(App());
+  runApp(const FocusableApp(child: App()));
 }
+
+
